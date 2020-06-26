28cee4fa8ec5487453cb8d83591d0ba3b614c4ce
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-26 20:21:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,16 @@ class Student < InteractiveRecord
     self.to_s.downcase.pluralize
   end
 
+
+  self.column_names.each do |col_name|
+      attr_accessor col_name.to_sym
+    end
+
+  def initialize(hash = {})
+    hash.each do |key, value|
+     self.send("#{key}=", value)
+    end
+  end
   def self.column_names
     DB[:conn].results_as_hash = true
 
@@ -23,15 +33,6 @@ class Student < InteractiveRecord
      column_names.compact
   end
 
-  self.column_names.each do |col_name|
-      attr_accessor col_name.to_sym
-    end
-
-  def initialize(hash = {})
-    hash.each do |key, value|
-     self.send("#{key}=", value)
-    end
-  end
 
   def table_name_for_insert
     self.class.table_name
