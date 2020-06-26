4f5b2271985bceec79ded6ad3daceefd97041cd9
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-26 20:22:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,20 @@ class Student < InteractiveRecord
     self.to_s.downcase.pluralize
   end
 
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "PRAGMA table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+
+    table_info.each do |column|
+    column_names << column["name"]
+    end
+
+     column_names.compact
+  end
 
   self.column_names.each do |col_name|
       attr_accessor col_name.to_sym
