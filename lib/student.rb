626f9d702a26af7ab533272b3036622bb55d0a3f
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-26 20:21:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,22 +18,6 @@ class Student < InteractiveRecord
      self.send("#{key}=", value)
     end
   end
-  
-  def self.column_names
-    DB[:conn].results_as_hash = true
-
-    sql = "PRAGMA table_info('#{table_name}')"
-
-    table_info = DB[:conn].execute(sql)
-    column_names = []
-
-    table_info.each do |column|
-    column_names << column["name"]
-    end
-
-     column_names.compact
-  end
-
 
   def table_name_for_insert
     self.class.table_name
